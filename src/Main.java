1fd5b240652a4e61548370a43c1fef16777863d1
Chase Martinez <chasemartinez22@gmail.com>
2022-09-07 18:19:26 -0500

    Feat: Solution to SquareAndSortArrays exercise

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] consecNums = {1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1};
@@ -5,5 +7,8 @@ public class Main {
 
         int[] evenDigitNums = {22, 321, 1, 3456, 123456, 12345, 32, 12345678};
         System.out.println("Numbers with even number of digits: " + EvenNumberDigits.evenNumberDigits(evenDigitNums));
+
+        int[] sortSquaredNums = {-7,-3,2,3,11};
+        System.out.println("Array Squared and sorted: " + Arrays.toString(SquaredSortedArray.squareAndSort(sortSquaredNums)));
     }
 }
