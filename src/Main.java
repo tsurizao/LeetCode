6c26cc8871568f5df4b0de8acbd8c6aba5d9c452
Chase Martinez <chasemartinez22@gmail.com>
2022-09-07 16:43:24 -0500

    Solution to exercise commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        int[] nums = {1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1};
+        System.out.println(MaxConsecutiveOnes.MaxConsecutiveOnes(nums));
     }
 }
---
+++ src/MaxConsecutiveOnes.java
@@ -0,0 +1,30 @@
+public class MaxConsecutiveOnes {
+//    Given a binary array nums, return the maximum number of consecutive 1's in the array.
+//    Example 1:
+//    Input: nums = [1,1,0,1,1,1]
+//    Output: 3
+//    Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+//
+//    Example 2:
+//    Input: nums = [1,0,1,1,0,1]
+//    Output: 2
+//    Constraints:
+//    1 <= nums.length <= 105
+//    nums[i] is either 0 or 1.
+
+    public static int MaxConsecutiveOnes(int[] nums) {
+        int consecutiveCount = 0;
+        int maxCount = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                consecutiveCount++;
+                if (maxCount < consecutiveCount) {
+                    maxCount = consecutiveCount;
+                }
+            } else {
+                consecutiveCount = 0;
+            }
+        }
+        return maxCount;
+    }
+}
