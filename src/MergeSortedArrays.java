fa0c5ea8c64cdab124dc37f5702cd2ce38e085e1
Chase Martinez <chasemartinez22@gmail.com>
2022-09-27 13:48:10 -0500

    Add complete and working solution to Merge Sorted Arrays exercise

---
+++ src/MergeSortedArrays.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class MergeSortedArrays {
+//
+//    You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+//
+//    Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+//
+//    The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        // first loop handles combining arrays
+        for (int i : nums2) {
+            nums1[m] = i;
+            m++;
+        }
+        // second loop handles organizing numbers
+        for (int i = 0; i < nums1.length - 1; i++) {
+            int temp;
+            if (nums1[i] > nums1[i + 1]) {
+                temp = nums1[i + 1];
+                nums1[i + 1] = nums1[i];
+                nums1[i] = temp;
+            }
+        }
+        System.out.println(Arrays.toString(nums1));
+    }
+}
