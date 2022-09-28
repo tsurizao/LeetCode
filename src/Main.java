c08fe0e75efd248d0dcb06faf4b44c2a374a0815
Chase Martinez <chasemartinez22@gmail.com>
2022-09-28 15:22:47 -0500

    Working on solution to organizing duplicates in an array

--- src/Main.java
+++ src/Main.java
@@ -23,13 +23,11 @@ public class Main {
 //        int n = 3;
 //        MergeSortedArrays.merge(nums1, m, nums2, n);
 
-        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
-        int val = 2;
-        RemoveElement.removeElement(nums, val);
-//        Output: 5, nums = [0, 1, 4, 0, 3, _, _, _]
-//        Explanation:
-//        Your function should return k = 5,with the first five elements of nums containing 0, 0, 1, 3, and 4.
-//        Note that the five elements can be returned in any order.
-//        It does not matter what you leave beyond the returned k (hence they are underscores).
+//        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+//        int val = 2;
+//        RemoveElement.removeElement(nums, val);
+
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        RemoveDuplicates.removeDuplicates(nums);
     }
 }
