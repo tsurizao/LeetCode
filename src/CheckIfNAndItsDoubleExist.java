7f5597977916cf88ad316347baf3327c2db8b494
Chase Martinez <chasemartinez22@gmail.com>
2022-10-03 13:40:12 -0500

    Completed solution

--- src/CheckIfNAndItsDoubleExist.java
+++ src/CheckIfNAndItsDoubleExist.java
@@ -4,10 +4,10 @@ public class CheckIfNAndItsDoubleExist {
     // Given an array arr of integers, check if there exist two indices i and j such that :
 
     public static boolean checkIfExists(int[] arr) {
-        for (int i : arr) {
-            for (int j : arr) {
-                if(i != j){
-                    if (i == 2 * j)
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (i != j) {
+                    if (arr[i] == 2 * arr[j])
                         return true;
                 }
             }
--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         If an array contains a number and its double, return true, else false
         EX. If an array contains 4 and 8 return true.
         */
-        int[] numbers = {-20, 8, -6, -14, 0, -19, 14, 4};
+        int[] numbers = {3, 1, 7, 11};
         System.out.println(CheckIfNAndItsDoubleExist.checkIfExists(numbers));
     }
 }
