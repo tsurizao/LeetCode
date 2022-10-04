62c1385c8bd56797f3e19ceb3f1acbc67bfa6efc
Chase Martinez <chasemartinez22@gmail.com>
2022-10-04 15:13:35 -0500

    Working solution for ValidMountainArray

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,9 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         // Using this space to test solution classes
+        int[] arr = {17, 5, 20, 3, 1};
 
+        System.out.println(ValidMountainArray.validMountainArray(arr));
 
-        /*
-        If an array contains a number and its double, return true, else false
-        EX. If an array contains 4 and 8 return true.
-        */
-        int[] numbers = {3, 1, 7, 11};
-        System.out.println(CheckIfNAndItsDoubleExist.checkIfExists(numbers));
     }
 }
