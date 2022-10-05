18967b5168e36f4a4187d1b172c73193727aba69
Chase Martinez <chasemartinez22@gmail.com>
2022-10-05 13:47:57 -0500

    This is a working solution to the exercise

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,5 @@ public class Main {
     public static void main(String[] args) {
         // Using this space to test solution classes
 
-        int[] arr = {17, 18, 5, 4, 6, 1};
-        ReplaceWithGreatestElement.replaceElement(arr);
     }
 }
