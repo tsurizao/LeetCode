f8a567f34291ae9f8f3c66fdf1a446bd9f01347f
Chase Martinez <chasemartinez22@gmail.com>
2022-10-10 13:41:06 -0500

    Removed printlns for testing purposes, changed for sorting algorithm to use Java built in sorting method instead

--- src/HeightChecker.java
+++ src/HeightChecker.java
@@ -8,26 +8,14 @@ public class HeightChecker {
 //
 //    Return the number of indices where heights[i] != expected[i].
     public static int heightChecker(int[] heights) {
-        System.out.println(Arrays.toString(heights));
         int[] original = heights.clone();
+        Arrays.sort(heights);
         int n = 0;
-        for (int i = 0; i < heights.length; i++) {
-            for (int j = i + 1; j < heights.length; j++) {
-                int temp;
-                if (heights[i] > heights[j]) {
-                    temp = heights[i];
-                    heights[i] = heights[j];
-                    heights[j] = temp;
-                }
-            }
-        }
         for (int i = 0; i < heights.length; i++) {
             if (heights[i] != original[i]) {
                 n++;
             }
         }
-        System.out.println(Arrays.toString(original));
-        System.out.println(Arrays.toString(heights));
         return n;
     }
 }
