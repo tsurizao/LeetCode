433a2cd56c222919bed513d01bb4c768fc4531e3
Chase Martinez <chasemartinez22@gmail.com>
2022-10-10 15:11:54 -0500

    Feat: Completed solution for returning the third max number, if exists.

--- src/ThirdMaxNumber.java
+++ src/ThirdMaxNumber.java
@@ -5,21 +5,12 @@ public class ThirdMaxNumber {
     //    Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
 
     public static int thirdMax(int[] nums) {
-        Arrays.sort(nums);
-        int k = 0;
-        int number = nums[nums.length - 1];
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (nums.length < 3) {
-                return nums[nums.length - 1];
-            }
-            if (k == 3) {
-                break;
-            }
-            if (nums[i] < number) {
-                k++;
-                number = nums[i];
-            }
+        int[] newNums = Arrays.stream(nums).distinct().toArray();
+        Arrays.sort(newNums);
+        int number = newNums[newNums.length - 1];
+        if(newNums.length < 3){
+            return number;
         }
-        return number;
+        return newNums[newNums.length - 3];
     }
 }
