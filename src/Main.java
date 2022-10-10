77af691adb6c6440064e573fba7440f05d5a76ef
Chase Martinez <chasemartinez22@gmail.com>
2022-10-10 14:59:49 -0500

    Adding solution attempt

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args) {
         // Using this space to test solution classes
 
+//        int[] nums = {3, 2, 1}; // output 1
+//        int[] nums = {1, 2}; // output 2
+//        int[] nums = {2, 2, 3, 1}; // output 1
+        int[] nums = {1, 1, 2}; // output 2
+        System.out.println("Output: " + ThirdMaxNumber.thirdMax(nums));
+
     }
 }
--- src/ThirdMaxNumber.java
+++ src/ThirdMaxNumber.java
@@ -1,9 +1,25 @@
+import java.util.Arrays;
+
 public class ThirdMaxNumber {
 
     //    Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
 
     public static int thirdMax(int[] nums) {
-        int count = 0;
-        return 0;
+        Arrays.sort(nums);
+        int k = 0;
+        int number = nums[nums.length - 1];
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums.length < 3) {
+                return nums[nums.length - 1];
+            }
+            if (k == 3) {
+                break;
+            }
+            if (nums[i] < number) {
+                k++;
+                number = nums[i];
+            }
+        }
+        return number;
     }
 }
