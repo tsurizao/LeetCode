04c3fde3406b987e5c42690d68553089f4d20cac
Chase Martinez <chasemartinez22@gmail.com>
2022-10-10 16:21:23 -0500

    Adding current progress, not a working solution to FindDisappearedNumbers

---
+++ src/FindDisappearedNumbers.java
@@ -0,0 +1,24 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindDisappearedNumbers {
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> missingNumbers = new ArrayList<>();
+        Arrays.sort(nums);
+        int[] distinctNums = Arrays.stream(nums.clone()).distinct().toArray();
+        System.out.println("Original Array" + Arrays.toString(nums));
+        System.out.println("Sorted Distinct Array" + Arrays.toString(distinctNums));
+        for (int i = distinctNums[0]; i < nums[nums.length - 1]; i++) {
+            for (int j = i + 1; j < nums[nums.length - 1]; j++) {
+                if (j == i) {
+                    break;
+                }
+                missingNumbers.add(j);
+            }
+        }
+        System.out.println("disappeared numbers" + missingNumbers);
+        return missingNumbers;
+    }
+}
