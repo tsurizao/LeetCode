e33c096a5ea36fac0bcb8b3219d49cec8e9c8f88
Chase Martinez <chasemartinez22@gmail.com>
2022-10-11 15:20:40 -0500

    Works with all but one test, where array length is greater than 50,000 or possibly more.

--- src/FindDisappearedNumbers.java
+++ src/FindDisappearedNumbers.java
@@ -1,28 +1,31 @@
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class FindDisappearedNumbers {
 
-    // TODO this is currently not working... like at all
     public static List<Integer> findDisappearedNumbers(int[] nums) {
-        Arrays.sort(nums);
+        System.out.println("Original" + Arrays.toString(nums));
         List<Integer> missingNumbers = new ArrayList<>();
-        int[] distinctNums = Arrays.stream(nums).distinct().toArray();
-        for (int i = 0; i < distinctNums.length; i++) {
-            missingNumbers.add(distinctNums[i]);
+        if (nums.length <= 1 || nums.length >= Math.pow(10, 5)) {
+            return missingNumbers;
         }
-        for (int i = nums[0]; i <= nums.length; i++) {
-            if(missingNumbers.contains(i)){
+        for (int num : nums) {
+            if (!missingNumbers.contains(num)) {
+                missingNumbers.add(num);
+            }
+        }
+        System.out.println("Distinct" + missingNumbers);
+        for (int i = 1; i <= nums.length; i++) {
+            if (missingNumbers.contains(i)) {
                 missingNumbers.remove((Integer) i);
             } else {
                 missingNumbers.add(i);
             }
         }
-        System.out.println("Original" + Arrays.toString(nums));
-        System.out.println("Distinct" + Arrays.toString(distinctNums));
-        System.out.println("List" + missingNumbers);
+        System.out.println("Missing" + missingNumbers);
         return missingNumbers;
     }
 }
