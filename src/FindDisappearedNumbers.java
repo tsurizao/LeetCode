60a9dc517aab9f0d411cc0d1973d9470adb76d80
Chase Martinez <chasemartinez22@gmail.com>
2022-10-11 14:40:09 -0500

    Testing current interation of solution

--- src/FindDisappearedNumbers.java
+++ src/FindDisappearedNumbers.java
@@ -7,20 +7,22 @@ public class FindDisappearedNumbers {
 
     // TODO this is currently not working... like at all
     public static List<Integer> findDisappearedNumbers(int[] nums) {
-        List<Integer> missingNumbers = new ArrayList<>();
         Arrays.sort(nums);
-        int[] distinctNums = Arrays.stream(nums.clone()).distinct().toArray();
-        System.out.println("Original Array" + Arrays.toString(nums));
-        System.out.println("Sorted Distinct Array" + Arrays.toString(distinctNums));
-        for (int i = distinctNums[0]; i < nums[nums.length - 1]; i++) {
-            for (int j = i + 1; j < nums[nums.length - 1]; j++) {
-                if (j == i) {
-                    break;
-                }
-                missingNumbers.add(j);
+        List<Integer> missingNumbers = new ArrayList<>();
+        int[] distinctNums = Arrays.stream(nums).distinct().toArray();
+        for (int i = 0; i < distinctNums.length; i++) {
+            missingNumbers.add(distinctNums[i]);
+        }
+        for (int i = nums[0]; i <= nums.length; i++) {
+            if(missingNumbers.contains(i)){
+                missingNumbers.remove((Integer) i);
+            } else {
+                missingNumbers.add(i);
             }
         }
-        System.out.println("disappeared numbers" + missingNumbers);
+        System.out.println("Original" + Arrays.toString(nums));
+        System.out.println("Distinct" + Arrays.toString(distinctNums));
+        System.out.println("List" + missingNumbers);
         return missingNumbers;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         // Using this space to test solution classes
 
-        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
-//        int[] nums = {1, 1};
+//        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+        int[] nums = {1, 1};
         FindDisappearedNumbers.findDisappearedNumbers(nums);
     }
 }
