dc25b826946d499c4849ad8e4f3b7099e1ac2667
Chase Martinez <chasemartinez22@gmail.com>
2022-10-13 14:58:28 -0500

    Solution works on all but arrays up to and over 50,000 objects

--- src/FindDisappearedNumbers.java
+++ src/FindDisappearedNumbers.java
@@ -5,20 +5,29 @@ import java.util.List;
 public class FindDisappearedNumbers {
 
     public static List<Integer> findDisappearedNumbers(int[] nums) {
-        //TODO still working on a solution, done for day
+        //TODO still working on a solution
         List<Integer> missingNumbers = new ArrayList<>();
-        Arrays.sort(nums);
-        System.out.println("nums" + Arrays.toString(nums));
-        int k = 1;
+        System.out.println("Numbers Found: " + Arrays.toString(nums));
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
-                if (nums[j] == k) {
-                    nums[j] = 1;
-                }
+            if (i >= 99999) {
+                break;
             }
-            k++;
+            if (!missingNumbers.contains(nums[i])) {
+                missingNumbers.add(nums[i]);
+            }
         }
-        System.out.println("numbersFound" + Arrays.toString(nums));
+        missingNumbers.sort(Integer::compareTo);
+        System.out.println("Missing" + missingNumbers);
+        for (int i = 1; i <= nums.length; i++) {
+            if (i > 99999) {
+                break;
+            }
+            if (missingNumbers.contains(i)) {
+                missingNumbers.remove((Integer) i);
+            } else {
+                missingNumbers.add(i);
+            }
+        }
         System.out.println("Missing" + missingNumbers);
         return missingNumbers;
     }
