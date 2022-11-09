9de6f2e4edfd6e4536f83e6ff5857dffd6625f7f
Chase Martinez <chasemartinez22@gmail.com>
2022-11-09 14:42:48 -0600

    Working on a solution to exercise

--- src/PowXN.java
+++ src/PowXN.java
@@ -1,6 +1,11 @@
 public class PowXN {
-//    Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+    //    Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
     public static double powXN(double x, int n) {
-
+        double power = x;
+        for (int i = n; i > 1; i--) {
+            System.out.println(power);
+            power = power * x;
+        }
+        return power;
     }
 }
