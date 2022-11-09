5f2d60e00bd87db5c75892bdb6fc72d6cece7240
Chase Martinez <chasemartinez22@gmail.com>
2022-11-09 14:43:47 -0600

    Feat: Complete and performant solution to exercise, using Java built in Math.pow() method

--- src/PowXN.java
+++ src/PowXN.java
@@ -1,11 +1,6 @@
 public class PowXN {
     //    Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
     public static double powXN(double x, int n) {
-        double power = x;
-        for (int i = n; i > 1; i--) {
-            System.out.println(power);
-            power = power * x;
-        }
-        return power;
+        return Math.pow(x, n);
     }
 }
