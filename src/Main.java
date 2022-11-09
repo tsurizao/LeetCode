14441994ab7d3d0fc9863a3ac1e3d3e978647f31
Chase Martinez <chasemartinez22@gmail.com>
2022-11-09 14:45:08 -0600

    Removal of test code

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,5 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         // Using this space to test solution classes
-//        double x = 2.0;
-//        int n = 10;
-//        double x = 2.1;
-//        int n = 3;
-        double x = 2;
-        int n = -2;
-        System.out.println(PowXN.powXN(x, n));
     }
 }
