c2164f1d521a5ab32e68b938a17d8d8e4e155a0d
Chase Martinez <chasemartinez22@gmail.com>
2022-11-15 17:36:46 -0600

    Feat: Solution to exercise

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args) {
 //         Using this space to test solution classes
 //        String s = "42";
-        String s = "-42";
+//        String s = "-42";
+//        String s = "words and 987";
+//        String s = "-91283472332";
+        String s = "-+12";
         System.out.println(StringToIntAtoi.myAtoi(s));
     }
 }
--- src/StringToIntAtoi.java
+++ src/StringToIntAtoi.java
@@ -3,17 +3,31 @@ public class StringToIntAtoi {
         s = s.trim();
         String numString = "";
         int sign = 1;
-
+        if (s.equals("")) {
+            return 0;
+        }
         if (s.charAt(0) == '-') {
             sign = -1;
+            s = s.substring(1);
+        } else if (s.charAt(0) == '+') {
+            s = s.substring(1);
         }
-
         for (int i = 0; i < s.length(); i++) {
-            if(Character.isDigit(s.charAt(i))){
+            if (Character.isDigit(s.charAt(i))) {
                 numString += s.charAt(i);
+            } else {
+                break;
             }
         }
-
+        if (numString.equals("")) {
+            return 0;
+        }
+        if (Double.parseDouble(numString) > Integer.MAX_VALUE) {
+            if (sign == 1) {
+                return Integer.MAX_VALUE;
+            }
+            return Integer.MIN_VALUE;
+        }
         int num = Integer.parseInt(numString);
         return num * sign;
     }
