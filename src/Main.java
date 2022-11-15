dab8a658f5acc436b0d835866c0ec2fe6da7a65f
Chase Martinez <chasemartinez22@gmail.com>
2022-11-15 17:37:21 -0600

    Removed test code from Main class

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 //         Using this space to test solution classes
-//        String s = "42";
-//        String s = "-42";
-//        String s = "words and 987";
-//        String s = "-91283472332";
-        String s = "-+12";
-        System.out.println(StringToIntAtoi.myAtoi(s));
+        System.out.println();
     }
 }
