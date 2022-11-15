58ba65e209350a655506213b44b2aa2bfedfffbe
Chase Martinez <chasemartinez22@gmail.com>
2022-11-15 16:50:38 -0600

    Removed test code from main class

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,5 @@
 public class Main {
     public static void main(String[] args) {
 //         Using this space to test solution classes
-//        String[] strs = {"flower","flow","flight"};
-//        String[] strs = {"dog","racecar","car"};
-//        String[] strs = {"a"};
-//        String[] strs = {"", ""};
-//        String[] strs = {"", "b"};
-//        String[] strs = {"flower", "flower", "flower", "flower"};
-        String[] strs = {"ab", "a"};
-//        String[] strs = {"abca","aba","aaab"};
-//        String[] strs = {"reflower","flow","flight"};
-        System.out.println("Longest common prefix = " + LongestCommonPrefix.longestCommonPrefix(strs));
     }
 }
