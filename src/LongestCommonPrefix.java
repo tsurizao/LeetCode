177b7ffc398c1ded2ee7ee80c913541bd9e5b64c
Chase Martinez <chasemartinez22@gmail.com>
2022-11-15 16:50:03 -0600

    Feat: Exercise completion

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -2,43 +2,30 @@
 public class LongestCommonPrefix {
     public static String longestCommonPrefix(String[] strs) {
 
-        if (strs.length == 1) {
-            return strs[0];
-        }
-
-        for(int i = 0;i < strs.length - 1;i++){
-            if(strs[i].equals("")){
-                return strs[i];
-            }
-        }
-
         String longestPrefix = "";
         String smallestString = strs[0];
-        char letterToCheck;
         boolean exitLoop = false;
 
         for (String str : strs) {
-            if (str.length() < smallestString.length()) {
+            if (str.length() <= smallestString.length()) {
                 smallestString = str;
             }
         }
-        int k = 0;
-        for (int i = 0; i < strs.length - 1; i++) {
-            System.out.println("I: " + i + " --- K: ");
-            letterToCheck = strs[i].charAt(k);
-            for (int j = 0; j < smallestString.length() - 1; j++) {
-                System.out.println("J: " + j);
-                //loop through each letter of each word
-                if (letterToCheck != strs[k].charAt(j)) {
+
+        for (int i = 0; i < smallestString.length(); i++) {
+            System.out.println("Letter to check: " + strs[0].charAt(i));
+            for (int j = 0; j < strs.length; j++) {
+                System.out.println("Subloop " + j);
+                if(strs[0].charAt(i) != strs[j].charAt(i)){
                     exitLoop = true;
                     break;
                 }
             }
-            if (exitLoop) {
+            if(exitLoop){
                 break;
+            } else {
+                longestPrefix += strs[0].charAt(i);
             }
-            longestPrefix += letterToCheck;
-            k++;
         }
         return longestPrefix;
     }
--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,9 @@ public class Main {
 //        String[] strs = {"", ""};
 //        String[] strs = {"", "b"};
 //        String[] strs = {"flower", "flower", "flower", "flower"};
-//        String[] strs = {"ab", "a"};
+        String[] strs = {"ab", "a"};
 //        String[] strs = {"abca","aba","aaab"};
-        String[] strs = {"reflower","flow","flight"};
+//        String[] strs = {"reflower","flow","flight"};
         System.out.println("Longest common prefix = " + LongestCommonPrefix.longestCommonPrefix(strs));
     }
 }
