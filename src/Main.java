6ad51b733a18d0a1a8702f4acf85f5f9fc30b3ed
Chase Martinez <chasemartinez22@gmail.com>
2022-11-17 17:08:44 -0600

    Removed test code, ValidParentheses exercise requires additional research into Stack && LIFO

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
+
+    /**
+     * -=([{ Current unworkable exercises }])=-
+     * - ValidParentheses - need to research Stack / LIFO
+     */
+
     public static void main(String[] args) {
 //         Using this space to test solution classes
-//        String s = "()";
-        String s = "()[]()";
-//        String s = "(]";
-//        String s = "([)]";
 
-        System.out.println(ValidParentheses.isValid(s));
     }
 }
--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -1,30 +1,6 @@
 public class ValidParentheses {
     public static boolean isValid(String s) {
-        StringBuilder bracketOrder = new StringBuilder();
-        for (int i = 0; i < s.length() - 1; i++) {
-            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
-                bracketOrder.append(s.charAt(i));
-            }
-        }
-        System.out.println(s);
-        System.out.println(bracketOrder);
-
-        int k = s.length() - 1;
-        for (int i = 0; i < s.length() - 1; i++) {
-            System.out.println(s.charAt(i));
-            System.out.println(s.charAt(k));
-            if ((s.charAt(i) == '(' && s.charAt(k) == ')')) {
-                k--;
-            } else if (s.charAt(i) == '[' && s.charAt(k) == ']') {
-                k--;
-            } else if (s.charAt(i) == '{' && s.charAt(k) == '}') {
-                k--;
-            } else if(s.charAt(i) == s.charAt(i + 1)){
-                continue;
-            } else {
-                return false;
-            }
-        }
-        return true;
+        // return once Stack / LIFO has been researched
+        return false;
     }
 }
