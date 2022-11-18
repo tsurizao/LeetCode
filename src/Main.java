6e05bb500823025f8b1624967f27633f25f47945
Chase Martinez <chasemartinez22@gmail.com>
2022-11-18 15:37:40 -0600

    Removed testing code from Main class

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
 //         Using this space to test solution classes
-//        int[] nums = {1, 3, 5, 6}; int target = 5;
-//        int[] nums = {1, 3, 5, 6}; int target = 2;
-//        int[] nums = {1, 3, 5, 6}; int target = 7;
-//        int[] nums = {1, 2, 4, 6, 7, 8, 12, 45}; int target = 99;
-//        int[] nums = {1}; int target = 2;
-        int[] nums = {1, 3}; int target = 2;
-        System.out.println(SearchInputPosition.searchInsert(nums, target));
+
     }
 }
