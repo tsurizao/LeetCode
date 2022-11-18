93d65f2c846376db254cbe10b2ccdf0ea0d36662
Chase Martinez <chasemartinez22@gmail.com>
2022-11-18 15:53:35 -0600

    Feat: completion of LengthOfLastWord exercise

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,11 @@ public class Main {
     public static void main(String[] args) {
 //         Using this space to test solution classes
 
+        String s = "Hello World";
+//        String s = "   fly me   to   the moon  ";
+//        String s = "luffy is still joyboy";
+
+        System.out.println(LengthOfLastWord.lengthOfLastWord(s));
+
     }
 }
