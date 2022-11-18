754f1486e591d60ceeb758a9a405bfbd63b3ee67
Chase Martinez <chasemartinez22@gmail.com>
2022-11-18 15:16:38 -0600

    Working on Solution to SearchingInputPosition exercise

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,12 @@ public class Main {
 
     public static void main(String[] args) {
 //         Using this space to test solution classes
+//        int[] nums = {1, 3, 5, 6}; int target = 5;
+        int[] nums = {1, 3, 5, 6}; int target = 2;
+//        int[] nums = {1, 3, 5, 6}; int target = 7;
+//        int[] nums = {1, 2, 4, 6, 7, 8, 12, 45}; int target = 99;
 
+
+        System.out.println(SearchInputPosition.searchInsert(nums, target));
     }
 }
---
+++ src/SearchInputPosition.java
@@ -0,0 +1,20 @@
+public class SearchInputPosition {
+    // Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+    //You must write an algorithm with O(log n) runtime complexity.
+
+    public static int searchInsert(int[] nums, int target) {
+        int location = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] == target){
+                return i;
+            }
+            if(i == nums.length - 2 && nums[i] < target){
+                return i + 2;
+            }
+            if(nums[i] < target){
+                location++;
+            }
+        }
+        return location;
+    }
+}
