d791ac0abb9e35435b44c0ec68fe28aa5752caab
Chase Martinez <chasemartinez22@gmail.com>
2022-11-21 18:07:45 -0600

    Feat: All exercise tests passed, completed solution

--- src/MergeTwoSortedLists.java
+++ src/MergeTwoSortedLists.java
@@ -1,6 +1,9 @@
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.List;
+
+import static java.util.Objects.isNull;
 
 // You are given the heads of two sorted linked lists list1 and list2.
 // Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
@@ -26,38 +29,16 @@ public class MergeTwoSortedLists {
     }
 
     public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if(list2 == null){
+            return list1;
+        }
+
         LinkedList<ListNode> combinedList = new LinkedList<>();
-        boolean isFirst = true;
-        ListNode nodeToCheck = new ListNode(list1.val, list1.next);
-
-        while (true) {
-            if (isFirst) {
-                combinedList.add(nodeToCheck);
-                isFirst = false;
-            } else {
-                if (nodeToCheck.next == null) {
-                    break;
-                }
-                nodeToCheck = new ListNode(nodeToCheck.next.val, nodeToCheck.next.next);
-                combinedList.add(nodeToCheck);
-            }
-        }
-
-        nodeToCheck = new ListNode(list2.val, list2.next);
-        isFirst = true;
-        while (true) {
-            if (isFirst) {
-                combinedList.add(nodeToCheck);
-                isFirst = false;
-            } else {
-                if (nodeToCheck.next == null) {
-                    break;
-                }
-                nodeToCheck = new ListNode(nodeToCheck.next.val, nodeToCheck.next.next);
-                combinedList.add(nodeToCheck);
-            }
-        }
-
+        combineListNodes(list1, combinedList);
+        combineListNodes(list2, combinedList);
         combinedList.sort(Comparator.comparingInt(o -> o.val));
         for (int i = 0; i < combinedList.size() - 1; i++) {
             combinedList.get(i).next = combinedList.get(i + 1);
@@ -66,5 +47,26 @@ public class MergeTwoSortedLists {
             }
         }
         return combinedList.getFirst();
+    }
+
+    private static void combineListNodes(ListNode list, LinkedList<ListNode> combinedList) {
+        boolean isFirst;
+        ListNode nodeToCheck;
+        if (list != null) {
+            isFirst = true;
+            nodeToCheck = new ListNode(list.val, list.next);
+            while (true) {
+                if (isFirst) {
+                    combinedList.add(nodeToCheck);
+                    isFirst = false;
+                } else {
+                    if (nodeToCheck.next == null) {
+                        break;
+                    }
+                    nodeToCheck = new ListNode(nodeToCheck.next.val, nodeToCheck.next.next);
+                    combinedList.add(nodeToCheck);
+                }
+            }
+        }
     }
 }
