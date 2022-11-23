8445cdb55e20a6d47bcb47eae49634bab4b4cbdf
Chase Martinez <chasemartinez22@gmail.com>
2022-11-23 17:14:01 -0600

    Working on different approach to solution

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,9 @@ public class Main {
 //        int[] digits = {1, 2, 3};
 //        int[] digits = {9, 8, 7, 6, 5, 4, 3, 2, 1};
 //        int[] digits = {9};
-        int[] digits = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+//        int[] digits = {4};
+//        int[] digits = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        int[] digits = {9, 9, 9};
         System.out.println(Arrays.toString(PlusOne.plusOne(digits)));
     }
 }
--- src/PlusOne.java
+++ src/PlusOne.java
@@ -1,41 +1,19 @@
+import java.util.Arrays;
+
 public class PlusOne {
 
     // You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
 
     //Increment the large integer by one and return the resulting array of digits.
     public static int[] plusOne(int[] digits) {
-        long number = 0;
-        int indices = digits.length;
-        int nineCount = 0;
-        for (int i = 0; i < digits.length; i++) {
-            number += digits[i];
-            if (digits[i] == 9) {
-                nineCount += 1;
+        digits[digits.length - 1] += 1;
+        for (int i = 0; i < digits.length - 1; i++) {
+            if (digits[i] == 10) {
+                System.out.println("yo");
+                digits[i] = 0;
+                digits[i + 1] += 1;
             }
-            if (i + 1 == digits.length) {
-                continue;
-            }
-            number *= 10;
         }
-        System.out.println("Number = " + number);
-        number += 1;
-        if (nineCount == indices) {
-            int[] newDigits = new int[indices + 1];
-            for (int i = indices; i >= 0; i--) {
-                newDigits[i] = (int) number % 10;
-                number /= 10;
-            }
-            return newDigits;
-        }
-        System.out.println("Number = " + number);
-        int[] newDigits = new int[indices];
-        for (int i = indices - 1; i >= 0; i--) {
-            System.out.println("number % 10 = " + number % 10);
-            newDigits[i] = (int) (number % 10);
-            System.out.println("newDigits[i] = " + newDigits[i]);
-            number /= 10;
-            System.out.println("Number after loop = " + number);
-        }
-        return newDigits;
+        return digits;
     }
 }
