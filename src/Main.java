712f75844036991739de49b4dad441393184bf28
Chase Martinez <chasemartinez22@gmail.com>
2022-11-28 14:50:57 -0600

    Removed test code from Main class

--- src/Main.java
+++ src/Main.java
@@ -10,14 +10,5 @@ public class Main {
     public static void main(String[] args) {
 //         Using this space to test solution classes
 
-//        int[] digits = {1, 2, 3};
-//        int[] digits = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-//        int[] digits = {9};
-//        int[] digits = {4};
-//        int[] digits = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-//        int[] digits = {9, 9, 9};
-//        int[] digits = {8, 9, 9, 9};
-        int[] digits = {9, 8, 9};
-        System.out.println(Arrays.toString(PlusOne.plusOne(digits)));
     }
 }
