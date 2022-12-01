1285eb550cf86c1f0becc74fa62bf2fba7d358a7
Chase Martinez <chasemartinez22@gmail.com>
2022-12-01 14:55:42 -0600

    REmoved testing code from Main Class

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 //         Using this space to test solution classes
-//        int x = 4;
-//        int x = 8;
-//        int x = 2147395599;
-        int x = 2147395600;
-        System.out.println(x + " X");
-        System.out.println(Integer.MAX_VALUE + " Max INT value");
-        System.out.println(SqrtX.mySqrt(x));
+
+
     }
 }
