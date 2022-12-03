6280b25c3d70624ac5c119fd2b29fb48d15ade72
Chase Martinez <chasemartinez22@gmail.com>
2022-12-02 19:36:24 -0600

    Removed test code from Main

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,5 @@ public class Main {
     public static void main(String[] args) {
 //         Using this space to test solution classes
 
-//        int[] nums = {2, 2, 1};
-//        int[] nums = {4, 1, 2, 1, 2};
-        int[] nums = {1};
-
-        System.out.println(SingleNumber.singleNumber(nums));
     }
 }
