3e6c59052b2be047796a7144a4861ceb5ac406e6
Chase Martinez <chasemartinez22@gmail.com>
2022-12-05 17:55:05 -0600

    Working on solution

--- src/ContainsDuplicate.java
+++ src/ContainsDuplicate.java
@@ -3,29 +3,37 @@ public class ContainsDuplicate {
     public static boolean containsDuplicate(int[] nums) {
 
         if (nums.length % 2 == 0) {
-            int halfIndex = nums.length / 2;
             for (int i = 0; i < nums.length / 2; i++) {
+                int k = nums.length / 2;
                 for (int j = 0; j < nums.length / 2; j++) {
+                    if(i == k){
+                        return true;
+                    }
                     if (i == j) {
                         continue;
                     }
-                    if (nums[i] == nums[j] || nums[i] == nums[halfIndex]) {
+                    if (nums[i] == nums[j] || nums[k] == nums[k + i]) {
                         return true;
                     }
                 }
-                halfIndex++;
             }
         } else {
-            int halfIndex = nums.length / 2;
             for (int i = 0; i < nums.length / 2; i++) {
+                int k = nums.length / 2 + 1;
                 for (int j = 0; j < nums.length / 2; j++) {
+                    System.out.println("i = " + nums[i] + " j = " + nums[j] + " halfIndex = " + nums[k]);
+                    k++;
                     if (i == j) {
-                        continue;
+                        if(nums[i] == nums[k]){
+                            return true;
+                        } else {
+                            continue;
+                        }
                     }
-                    if (nums[i] == nums[j] || nums[i] == nums[halfIndex]) {
+                    if (nums[i] == nums[j] || nums[i] == nums[k]) {
                         return true;
                     }
-                    halfIndex++;
+                    k++;
                 }
             }
         }
