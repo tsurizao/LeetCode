b3c763533a3337759e410db318ce5ddbe86d2b73
Chase Martinez <chasemartinez22@gmail.com>
2022-12-06 16:24:55 -0600

    Solution works, however, need to remove a nested loop to improve time complexity

--- src/ContainsDuplicate.java
+++ src/ContainsDuplicate.java
@@ -1,6 +1,17 @@
 public class ContainsDuplicate {
 
     public static boolean containsDuplicate(int[] nums) {
-
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if(i == j){
+                    break;
+                } else {
+                    if(nums[i] == nums[j]){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
