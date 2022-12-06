b5796301d5cb70bfcc31f2ca643e7f090766cee0
Chase Martinez <chasemartinez22@gmail.com>
2022-12-06 17:11:59 -0600

    Added instructions to exercise, after completion

--- src/ContainsDuplicate.java
+++ src/ContainsDuplicate.java
@@ -3,6 +3,8 @@ import java.util.Set;
 
 public class ContainsDuplicate {
 
+    //    Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
     public static boolean containsDuplicate(int[] nums) {
         Set<Integer> numberSet = new HashSet<>();
         for (int num : nums) {
