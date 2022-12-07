2489aec5645aae24f092b63254d2aefea0a5bf81
Chase Martinez <chasemartinez22@gmail.com>
2022-12-06 18:12:27 -0600

    Attempt at solving exercise ValidParentheses by removing each group of correct sequence and replacing with empty string

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+//        String s = "()";
+//        String s = "()[]{}";
+//        String s = "(]";
+        String s = "([])";
+        System.out.println(ValidParentheses.isValid(s));
     }
 }
--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -1,6 +1,38 @@
+import java.util.Locale;
+
 public class ValidParentheses {
     public static boolean isValid(String s) {
         // return once Stack / LIFO has been researched
+        int i = 0;
+        while (i < s.length() - 1) {
+            System.out.println(s.length() + " length left");
+            System.out.println(i + " i");
+            System.out.println(s.charAt(i) + " character at i");
+            System.out.println(s + " full string");
+            System.out.println("=====");
+            if (s.charAt(i) == '(' && s.charAt(i + 1) == ')') {
+                s = s.replace("()", "");
+                i = 0;
+                if (s.length() == 0) {
+                    return true;
+                }
+            }
+            if (s.charAt(i) == '[' && s.charAt(i + 1) == ']') {
+                s = s.replace("[]", "");
+                i = 0;
+                if (s.length() == 0) {
+                    return true;
+                }
+            }
+            if (s.charAt(i) == '{' && s.charAt(i + 1) == '}') {
+                s = s.replace("{}", "");
+                i = 0;
+                if (s.length() == 0) {
+                    return true;
+                }
+            }
+            i++;
+        }
         return false;
     }
 }
