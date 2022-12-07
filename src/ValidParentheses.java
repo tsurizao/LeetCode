b1ff119527ee09e87f606115c2bb068a5cb736d9
Chase Martinez <chasemartinez22@gmail.com>
2022-12-07 17:44:05 -0600

    Solution works with smaller Strings of parentheses types, however does not meet time requirements for larger strings such as over 100,000 characters

--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -3,36 +3,14 @@ import java.util.Locale;
 public class ValidParentheses {
     public static boolean isValid(String s) {
         // return once Stack / LIFO has been researched
-        int i = 0;
-        while (i < s.length() - 1) {
-            System.out.println(s.length() + " length left");
-            System.out.println(i + " i");
-            System.out.println(s.charAt(i) + " character at i");
-            System.out.println(s + " full string");
-            System.out.println("=====");
-            if (s.charAt(i) == '(' && s.charAt(i + 1) == ')') {
-                s = s.replace("()", "");
-                i = 0;
-                if (s.length() == 0) {
-                    return true;
-                }
-            }
-            if (s.charAt(i) == '[' && s.charAt(i + 1) == ']') {
-                s = s.replace("[]", "");
-                i = 0;
-                if (s.length() == 0) {
-                    return true;
-                }
-            }
-            if (s.charAt(i) == '{' && s.charAt(i + 1) == '}') {
-                s = s.replace("{}", "");
-                i = 0;
-                if (s.length() == 0) {
-                    return true;
-                }
-            }
-            i++;
+        for (int i = 0; s.length() > 1; i++) {
+            System.out.println(s);
+            s = s.replace("()", "");
+            System.out.println(s);
+            s = s.replace("[]", "");
+            System.out.println(s);
+            s = s.replace("{}", "");
         }
-        return false;
+        return s.length() == 0;
     }
 }
