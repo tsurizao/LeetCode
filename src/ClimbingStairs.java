4b507dfb86b2627e16826747c2a3c03c45324418
Chase Martinez <chasemartinez22@gmail.com>
2022-12-12 14:46:23 -0600

    Starting from scratch

--- src/ClimbingStairs.java
+++ src/ClimbingStairs.java
@@ -6,25 +6,7 @@ public class ClimbingStairs {
     public static int climbStairs(int n) {
 
         int distinctWaysToClimb = 0;
-        int remainingSteps = n;
-        int numberOfTwos = n / 2;
-//        subtract by 2's until no longer able to, every loop reduce the amount of 2's to subtract
-        while (true) {
-            if (numberOfTwos > 0) {
-                for (int i = 0; i < numberOfTwos; i++) {
-                    remainingSteps -= 2;
-                    if (remainingSteps < 2) {
-                        break;
-                    }
-                }
-            }
-            distinctWaysToClimb++;
-            numberOfTwos--;
-            remainingSteps = n;
-            if (numberOfTwos == -1) {
-                break;
-            }
-        }
+
         return distinctWaysToClimb;
     }
 }
