1ffc1645800ba44ee2cfc6248642a13a270fa87e
Chase Martinez <chasemartinez22@gmail.com>
2022-12-12 13:17:40 -0600

    Removed test code from main for the ValidParentheses exercise.  Added Climbing Stairs exercise and basic method

---
+++ src/ClimbingStairs.java
@@ -0,0 +1,9 @@
+public class ClimbingStairs {
+//    You are climbing a staircase. It takes n steps to reach the top.
+//
+//    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+    public static int climbStairs(int n){
+        return -1;
+    }
+}
