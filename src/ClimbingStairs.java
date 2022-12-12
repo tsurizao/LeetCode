6c1b5151180e3573255604ca4c9788f202c418e8
Chase Martinez <chasemartinez22@gmail.com>
2022-12-12 13:44:54 -0600

    First attempt, not a working solution

--- src/ClimbingStairs.java
+++ src/ClimbingStairs.java
@@ -3,7 +3,36 @@ public class ClimbingStairs {
 //
 //    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
-    public static int climbStairs(int n){
-        return -1;
+    public static int climbStairs(int n) {
+
+        int distinctWaysToClimb = 0;
+        int remainingSteps = n;
+        int numberOfTwos = n / 2;
+//        subtract by 2's until no longer able to, every loop reduce the amount of 2's to subtract
+        while (true) {
+            if (numberOfTwos > 0) {
+                for (int i = 0; i < numberOfTwos; i++) {
+                    remainingSteps -= 2;
+                    if (remainingSteps == 1) {
+                        break;
+                    }
+                    if (remainingSteps == 0) {
+                        distinctWaysToClimb += 1;
+                    }
+                }
+            }
+            for (int i = 0; i < remainingSteps; i++) {
+                remainingSteps -= 1;
+                if (remainingSteps == 0) {
+                    distinctWaysToClimb++;
+                    break;
+                }
+            }
+            numberOfTwos--;
+            if (numberOfTwos < 0) {
+                break;
+            }
+        }
+        return distinctWaysToClimb;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,9 @@ public class Main {
      */
 
     public static void main(String[] args) {
-
+        System.out.println("Correct ways 1 -> " + ClimbingStairs.climbStairs(1));
+        System.out.println("Correct ways 2 -> " + ClimbingStairs.climbStairs(2));
+        System.out.println("Correct ways 3 -> " + ClimbingStairs.climbStairs(3));
+        System.out.println("Correct ways 5 -> " + ClimbingStairs.climbStairs(4));
     }
 }
