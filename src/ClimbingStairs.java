87ef1c42e8ec8ade8788d592209a7519dc3893f5
Chase Martinez <chasemartinez22@gmail.com>
2022-12-13 13:36:26 -0600

    Moving away from ClimbingStairs exercise until better grasp on concept

--- src/ClimbingStairs.java
+++ src/ClimbingStairs.java
@@ -4,9 +4,6 @@ public class ClimbingStairs {
 //    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
     public static int climbStairs(int n) {
-
-        int distinctWaysToClimb = 0;
-
-        return distinctWaysToClimb;
+        return -1;
     }
 }
