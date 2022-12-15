2b1273fc3c8c1b14df06ebcf4e658875cc5b0ce7
Chase Martinez <chasemartinez22@gmail.com>
2022-12-15 14:43:39 -0600

    Solution attempt, does not work with single character String

--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -1,4 +1,5 @@
 import java.util.Locale;
+import java.util.Stack;
 
 public class ValidParentheses {
     //        Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
@@ -9,7 +10,19 @@ public class ValidParentheses {
 //                Open brackets must be closed in the correct order.
 //        Every close bracket has a corresponding open bracket of the same type.
     public static boolean isValid(String s) {
-
-        return true;
+        Stack<Character> parenBank = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println("i: " + s.charAt(i) + " ||| " + "bank: " + parenBank);
+            if (s.charAt(i) == ')' && parenBank.peek() == '(') {
+                parenBank.pop();
+            } else if (s.charAt(i) == '}' && parenBank.peek() == '{') {
+                parenBank.pop();
+            } else if (s.charAt(i) == ']' && parenBank.peek() == '[') {
+                parenBank.pop();
+            } else {
+                parenBank.push(s.charAt(i));
+            }
+        }
+        return parenBank.isEmpty();
     }
 }
