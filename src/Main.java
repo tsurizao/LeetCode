eabcf90ddeab38f536ef9107cc03a4c0e67b580a
Chase Martinez <chasemartinez22@gmail.com>
2022-12-15 15:11:04 -0600

    Removed testing code from Main, updated notes to remove unworkable exercise ValidParentheses

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,11 @@ public class Main {
 
     /**
      * -=([{ Current unworkable exercises }])=-
-     * - ValidParentheses - need to research Stack / LIFO
+     * [NONE]
      */
 
     public static void main(String[] args) {
 
-        System.out.println("1");
-        System.out.println(ValidParentheses.isValid(")"));
-        System.out.println("2");
-        System.out.println(ValidParentheses.isValid("(]"));
-        System.out.println("3");
-        System.out.println(ValidParentheses.isValid("()"));
-        System.out.println("4");
-        System.out.println(ValidParentheses.isValid("(){}[]"));
-        System.out.println("5");
-        System.out.println(ValidParentheses.isValid("({}[(()){}])"));
-        System.out.println("6");
-        System.out.println(ValidParentheses.isValid("{[]({}[(()){}])({}[(()){}])}"));
-        System.out.println("7");
-        System.out.println(ValidParentheses.isValid("(((((((((((((((((((((((((([{(((([](){})))){{}}}]))))))))))))))))))))))))))"));
+
     }
 }
