d3e4cd9edbc02aa6e5281657713c5742713651bb
Chase Martinez <chasemartinez22@gmail.com>
2022-12-19 16:54:16 -0600

    First iteration with a return answer, works for all binary slots except the 1's place

--- src/AddBinary.java
+++ src/AddBinary.java
@@ -2,7 +2,7 @@ public class AddBinary {
 
     //        Given two binary strings a and b, return their sum as a binary string.
 
-    public static String addBinary(String a, String b){
+    public static String addBinary(String a, String b) {
 
         // Parse binary numbers from String inputs
         int binA = Integer.parseInt(a);
@@ -15,7 +15,8 @@ public class AddBinary {
         // Multiplier multiplied by 2 everytime the loop iterates.  Simulates binary values 1, 2, 4, 8, 16 etc.
         int multiplier = 1;
 
-        for(int i = a.length(); i > 0;i--){
+        // Loop to convert binary integers to number integers
+        for (int i = a.length(); i > 0; i--) {
 
             // Extracts the last number (0 or 1) and multiplies it to the multiplier everytime the loop runs
             // Adds the values to numA and numB respectively
@@ -28,13 +29,28 @@ public class AddBinary {
             multiplier *= 2;
         }
 
+        // Variable that holds the sum of both converted binary numbers
         int numTotal = numA + numB;
+
+        // Printing to check that everything works so far (Works as intended thus far)
         System.out.println("A = " + numA + " /// B = " + numB);
         System.out.println("Total = " + numTotal);
+        System.out.println("Multiplier = " + multiplier);
 
-        for(int i = 0;i > 0;i++){
-            
+        // Place to store sum as a binary String
+        String binaryTotal = "";
+
+
+        // Loop to revert to number total into a binary String
+        for (int i = 0; multiplier > 1; i++) {
+            if (numTotal - multiplier >= 0) {
+                binaryTotal += "1";
+                numTotal -= multiplier;
+            } else {
+                binaryTotal += "0";
+            }
+            multiplier /= 2;
         }
-        return "lul";
+        return binaryTotal;
     }
 }
