e132930e38cab65a969fe86364b0a4f493d43ac6
Chase Martinez <chasemartinez22@gmail.com>
2022-12-27 15:07:21 -0600

    Feat: Working solution to exercise RemoveDuplicatesFromSortedList

--- src/RemoveDuplicatesFromSortedList.java
+++ src/RemoveDuplicatesFromSortedList.java
@@ -23,32 +23,14 @@ public class RemoveDuplicatesFromSortedList {
 //    such that each element appears only once. Return the linked list sorted as well.
 
     public ListNode deleteDuplicates(ListNode head) {
-
-        // Returns ListNode with no fields populated if head is null
-        if (head == null) {
-            return new ListNode();
-        }
-        // Initialize a LinkedList and add the first node
-        LinkedList<ListNode> sortedList = new LinkedList<>();
-        sortedList.add(head);
-
-        // Iterate through input node and add all nodes, if any
-        for (int i = 0; sortedList.get(i).next != null; i++) {
-            sortedList.add(sortedList.get(i).next);
-        }
-
-        // nested loop will take and compare nodes at index i vs nodes at index j,
-        // if matched, i.next will be set to j.next which severs j from the chain
-        for (int i = 0; i < sortedList.size(); i++) {
-            if (sortedList.size() > 1) {
-                for (int j = i + 1; j < sortedList.size(); j++) {
-                    if (sortedList.get(i).val == sortedList.get(j).val) {
-                        sortedList.get(i).next = sortedList.get(j).next;
-                    }
-                }
+        ListNode node = head;
+        while (node != null && node.next != null) {
+            if (node.val == node.next.val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
             }
         }
-
-        return sortedList.get(0);
+        return head;
     }
 }
