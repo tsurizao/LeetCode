ddaf5a095a804d9c5187133003f9a75d884d6aff
Chase Martinez <chasemartinez22@gmail.com>
2023-01-04 13:41:17 -0600

    Feat: Complete working solution to exercise, practiced adding comments for more clarity, ended up using Stack for solution

--- src/BinaryTreeInorderTraversal.java
+++ src/BinaryTreeInorderTraversal.java
@@ -27,17 +27,30 @@ public class BinaryTreeInorderTraversal {
     }
 
     public List<Integer> inorderTraversal(TreeNode root) {
-        // Stack class/data structure -- Using to store values
-        Stack<TreeNode> valuesStack = new Stack<>();
-
-        // Creating new TreeNode object to traverse/iterate through
-        TreeNode node = root;
-
+        // Stack used to store current node being checked. Allows nodes to be stored
+        // keeping track of the order in which nodes are being traversed
+        Stack<TreeNode> valStack = new Stack<>();
+        // For storing node values to be returned per Inorder Traversal
         List<Integer> values = new ArrayList<>();
-        while (!valuesStack.empty()) {
-            System.out.println(valuesStack.peek().val);
-            values.add(valuesStack.peek().val);
-            valuesStack.pop();
+
+        // Each node is added to a Stack (if it isn't null) then each node.left is traversed
+        // If a node.left is found null, the value of node(root) is stored in an Arraylist
+        // Then node(root) is set to node.right.  The stack pops the top off, since it is an
+        // end point.  Process if repeated on node.right -- checks node.left != null etc etc
+        while(root != null || !valStack.empty()){
+            // Add to stack and set root to node.left, as long as node isn't null
+            if(root != null){
+                valStack.push(root);
+                root = root.left;
+            }
+            // If node.left is null, set node to top of stack, add the value of node(root) to
+            // arraylist, set node(root) to node.right, pop stack.
+            if(root == null){
+                root = valStack.peek();
+                values.add(root.val);
+                root = root.right;
+                valStack.pop();
+            }
         }
         return values;
     }
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     /**
      * -=([{ Current unworkable exercises }])=-
-     *
+     * NONE
      */
 
     public static void main(String[] args) {
