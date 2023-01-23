d7df5fd079cfb73551a63e733c2eb2a217efeab0
Chase Martinez <chasemartinez22@gmail.com>
2023-01-23 16:42:47 -0600

    Still working on solution, refactored to create method that redudes duplicate code

--- src/SameTree.java
+++ src/SameTree.java
@@ -25,59 +25,46 @@ public class SameTree {
     }
 
     // This is repurposed from BinaryTreeInorderTraversal coding challenge.
-    public boolean sameTreeInorderTraversal(SameTree.TreeNode rootA, SameTree.TreeNode rootB) {
-        // Stack used to store current node being checked. Allows nodes to be stored
-        // keeping track of the order in which nodes are being traversed
-        Stack<SameTree.TreeNode> valStack = new Stack<>();
-        // For storing node values to be returned per Inorder Traversal
+    public boolean sameTreeInorderTraversal(TreeNode p, TreeNode q) {
+        Stack<TreeNode> stackA = new Stack<>();
+        Stack<TreeNode> stackB = new Stack<>();
         List<Integer> valuesA = new ArrayList<>();
         List<Integer> valuesB = new ArrayList<>();
         boolean areSame = true;
 
-
-        // Each node is added to a Stack (if it isn't null) then each node.left is traversed
-        // If a node.left is found null, the value of node(root) is stored in an Arraylist
-        // Then node(root) is set to node.right.  The stack pops the top off, since it is an
-        // end point.  Process if repeated on node.right -- checks node.left != null etc etc
-        while (rootA != null || !valStack.empty()) {
-            // Add to stack and set root to node.left, as long as node isn't null
-            if (rootA != null) {
-                valStack.push(rootA);
-                rootA = rootA.left;
-            }
-            // If node.left is null, set node to top of stack, add the value of node(root) to
-            // arraylist, set node(root) to node.right, pop stack.
-            if (rootA == null) {
-                rootA = valStack.peek();
-                valuesA.add(rootA.val);
-                rootA = rootA.right;
-                valStack.pop();
-            }
-        }
-        while (rootB != null || !valStack.empty()) {
-            // Add to stack and set root to node.left, as long as node isn't null
-            if (rootB != null) {
-                valStack.push(rootB);
-                rootB = rootB.left;
-            }
-            // If node.left is null, set node to top of stack, add the value of node(root) to
-            // arraylist, set node(root) to node.right, pop stack.
-            if (rootB == null) {
-                rootB = valStack.peek();
-                valuesB.add(rootB.val);
-                rootB = rootB.right;
-                valStack.pop();
-            }
-        }
+        validNodes(p, (Stack<TreeNode>) stackA, valuesA);
+        validNodes(q, (Stack<TreeNode>) stackB, valuesB);
+        System.out.println("Values A size = " + valuesA.size());
+        System.out.println("Values B size = " + valuesB.size());
         if(valuesA.size() != valuesB.size()){
+            System.out.println("Checking sizes");
             return false;
         }
         for(int i = 0;i < valuesA.size();i++){
+            System.out.println(valuesA.get(i) + " " + valuesB.get(i));
             if (valuesA.get(i) != valuesB.get(i)) {
                 areSame = false;
                 break;
             }
         }
         return areSame;
+    }
+
+    private void validNodes(TreeNode q, Stack<TreeNode> valStackB, List<Integer> valuesB) {
+        while (q != null || !valStackB.empty()) {
+            if (q != null) {
+                valStackB.push(q);
+                q = q.left;
+            }
+            if (q == null) {
+                q = valStackB.peek();
+                valuesB.add(q.val);
+                q = q.right;
+                if(q == null){
+                    valuesB.add(0);
+                }
+                valStackB.pop();
+            }
+        }
     }
 }
