cdb474d87a119508761fc6d25ada2ca19ff757b3
Chase Martinez <chasemartinez22@gmail.com>
2023-01-24 14:24:00 -0600

    Solution using recursion works properly

--- src/SameTree.java
+++ src/SameTree.java
@@ -24,8 +24,12 @@ public class SameTree {
         }
     }
 
-    // This is repurposed from BinaryTreeInorderTraversal coding challenge.
+    // Simple solution that uses recursion, if at any point a method call
+    // returns false, the original method returns false as well.
     public boolean sameTree(TreeNode p, TreeNode q) {
-
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        System.out.println("P VAL = " + p.val + " Q VAL = " + q.val);
+        return p.val == q.val && sameTree(p.left, q.left) && sameTree(p.right, q.right);
     }
 }
