0815d9ec82f4f4d6bcd13a4a67e3d92322f84660
Chase Martinez <chasemartinez22@gmail.com>
2023-01-24 14:10:04 -0600

    Working from scratch, inorder traversal might not be the best answer

--- src/SameTree.java
+++ src/SameTree.java
@@ -25,46 +25,7 @@ public class SameTree {
     }
 
     // This is repurposed from BinaryTreeInorderTraversal coding challenge.
-    public boolean sameTreeInorderTraversal(TreeNode p, TreeNode q) {
-        Stack<TreeNode> stackA = new Stack<>();
-        Stack<TreeNode> stackB = new Stack<>();
-        List<Integer> valuesA = new ArrayList<>();
-        List<Integer> valuesB = new ArrayList<>();
-        boolean areSame = true;
+    public boolean sameTree(TreeNode p, TreeNode q) {
 
-        validNodes(p, (Stack<TreeNode>) stackA, valuesA);
-        validNodes(q, (Stack<TreeNode>) stackB, valuesB);
-        System.out.println("Values A size = " + valuesA.size());
-        System.out.println("Values B size = " + valuesB.size());
-        if(valuesA.size() != valuesB.size()){
-            System.out.println("Checking sizes");
-            return false;
-        }
-        for(int i = 0;i < valuesA.size();i++){
-            System.out.println(valuesA.get(i) + " " + valuesB.get(i));
-            if (valuesA.get(i) != valuesB.get(i)) {
-                areSame = false;
-                break;
-            }
-        }
-        return areSame;
-    }
-
-    private void validNodes(TreeNode q, Stack<TreeNode> valStackB, List<Integer> valuesB) {
-        while (q != null || !valStackB.empty()) {
-            if (q != null) {
-                valStackB.push(q);
-                q = q.left;
-            }
-            if (q == null) {
-                q = valStackB.peek();
-                valuesB.add(q.val);
-                q = q.right;
-                if(q == null){
-                    valuesB.add(0);
-                }
-                valStackB.pop();
-            }
-        }
     }
 }
