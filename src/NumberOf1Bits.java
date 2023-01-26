c1d4d63f848294bc8989dd131bfba1614ea4d0dd
Chase Martinez <chasemartinez22@gmail.com>
2023-01-26 15:47:19 -0600

    Add new exercise NumberOf1Bits

---
+++ src/NumberOf1Bits.java
@@ -0,0 +1,22 @@
+public class NumberOf1Bits {
+
+    /**
+     Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+
+     Note:
+
+     Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+     In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
+     */
+
+    public static int hammingWeight(int n){
+        int x = 0;
+        for(int i = 0;i < n;i++){
+            if(n % 10 != 0){
+                x++;
+            }
+            n /= 10;
+        }
+        return x;
+    }
+}
