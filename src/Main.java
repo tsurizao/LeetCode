db64706e01596a42c6bd9e30056c8c5e18028e3a
Chase Martinez <chasemartinez22@gmail.com>
2023-01-27 14:01:12 -0600

    Add: BestTimeToBuyAndSellStock exercise with directions and first iteration of a solution.

--- src/Main.java
+++ src/Main.java
@@ -13,12 +13,14 @@ public class Main {
 //        System.out.println(BinaryTreeInorderTraversal.inorderTraversal());
 //        System.out.println(SameTree.sameTree());
 
-        //actual input 00000000000000000000000010000000
-        System.out.println(NumberOf1Bits.hammingWeight(11));
-        //actual input 00000000000000000000000010000000
-        System.out.println(NumberOf1Bits.hammingWeight(128));
-        //actual input 11111111111111111111111111111101
-        System.out.println(NumberOf1Bits.hammingWeight(-3));
+//        //actual input 00000000000000000000000010000000
+//        System.out.println(NumberOf1Bits.hammingWeight(11));
+//        //actual input 00000000000000000000000010000000
+//        System.out.println(NumberOf1Bits.hammingWeight(128));
+//        //actual input 11111111111111111111111111111101
+//        System.out.println(NumberOf1Bits.hammingWeight(-3));
 
+        System.out.println(BestTimeToBuyAndSellStock.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(BestTimeToBuyAndSellStock.maxProfit(new int[]{7, 6, 4, 3, 1}));
     }
 }
