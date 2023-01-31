1faff106922b6dfb8f8f4e919259e022b0443ee0
Chase Martinez <chasemartinez22@gmail.com>
2023-01-31 16:17:47 -0600

    Current code to check conditionals

--- src/ClimbingStairs.java
+++ src/ClimbingStairs.java
@@ -5,6 +5,12 @@ public class ClimbingStairs {
 //    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
     public static int climbStairs(int n) {
-        return -1;
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        if(n % 2 == 0){
+            return n * 2;
+        } else {
+            return n * 2 + 1;
+        }
     }
 }
