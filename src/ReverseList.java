c6c5b15e89859bf7d7fa9752526eedce9c014343
Chase Martinez <chasemartinez22@gmail.com>
2023-02-02 14:44:43 -0600

    Feat: New solution is much more efficient, perfomant run time and space complexity

--- src/ReverseList.java
+++ src/ReverseList.java
@@ -30,29 +30,47 @@ public class ReverseList {
     }
 
     public static ListNode reverseList(ListNode head) {
-        if(head == null){
-            return null;
-        }
-        ListNode node = head;
-        ArrayList<ListNode> nodeList = new ArrayList<>();
-
-        // Add each node to an ArrayList
-        while (node != null) {
-            nodeList.add(node);
-            node = node.next;
-        }
-
-        //Reverse the ArrayList and update each node
-        Collections.reverse(nodeList);
-        for (int i = 0; i < nodeList.size(); i++) {
-            // Setting the last node's .next to null
-            if(nodeList.get(i) == nodeList.get(nodeList.size() - 1)){
-                nodeList.get(i).next = null;
-                continue;
+        ListNode previousNode = null;
+        ListNode currentNode = head;
+        while (true) {
+            // Set aside the next node
+            ListNode temporaryNode = currentNode.next;
+            // Set current.next to previous node (null on first loop)
+            currentNode.next = previousNode;
+            // Previous now set to current for next loop
+            previousNode = currentNode;
+            // The new current will be the temp we set aside at loop beginning
+            currentNode = temporaryNode;
+            // Exit condition
+            if (currentNode == null) {
+                break;
             }
-            // Setting each node's .next to the next node
-            nodeList.get(i).next = nodeList.get(i + 1);
         }
-        return nodeList.get(0);
+        // Upon completion of the while loop, previous is the tail, which
+        // now that all the nodes.nexts have been reverse the tail is
+        // technically the head
+        return previousNode;
+
+
+        // This solution works but is not as efficient as the one above
+//        if(head == null){
+//            return null;
+//        }
+//        ListNode node = head;
+//        ArrayList<ListNode> nodeList = new ArrayList<>();
+//        while (node != null) {
+//            nodeList.add(node);
+//            node = node.next;
+//        }
+//        Collections.reverse(nodeList);
+//        for (int i = 0; i < nodeList.size(); i++) {
+//            if(nodeList.get(i) == nodeList.get(nodeList.size() - 1)){
+//                nodeList.get(i).next = null;
+//                continue;
+//            }
+//            nodeList.get(i).next = nodeList.get(i + 1);
+//        }
+//        return nodeList.get(0);
+
     }
 }
