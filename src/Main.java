6f5a531e92a67d72b8e97146425f50c7a876e9c3
Chase Martinez <chasemartinez22@gmail.com>
2023-02-06 15:07:02 -0600

    Add: new exercise ShuffleTheArray.java class

--- src/Main.java
+++ src/Main.java
@@ -10,26 +10,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        System.out.println(BinaryTreeInorderTraversal.inorderTraversal());
-//        System.out.println(SameTree.sameTree());
+        System.out.println(Arrays.toString(ShuffleTheArray.shuffle(new int[]{1, 1, 2, 2}, 2)));
+        System.out.println(Arrays.toString(ShuffleTheArray.shuffle(new int[]{2, 5, 1, 3, 4, 7}, 3)));
+        System.out.println(Arrays.toString(ShuffleTheArray.shuffle(new int[]{1, 2, 3, 4, 4, 3, 2, 1}, 4)));
 
-//        //actual input 00000000000000000000000010000000
-//        System.out.println(NumberOf1Bits.hammingWeight(11));
-//        //actual input 00000000000000000000000010000000
-//        System.out.println(NumberOf1Bits.hammingWeight(128));
-//        //actual input 11111111111111111111111111111101
-//        System.out.println(NumberOf1Bits.hammingWeight(-3));
 
-//        System.out.println(BestTimeToBuyAndSellStock.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
-//        System.out.println(BestTimeToBuyAndSellStock.maxProfit(new int[]{7, 6, 4, 3, 1}));
-//        System.out.println(BestTimeToBuyAndSellStock.maxProfit(new int[]{1, 2}));
-
-//        System.out.println("2 should have 2 ways = " + ClimbingStairs.climbStairs(2));
-//        System.out.println("3 should have 3 ways = " + ClimbingStairs.climbStairs(3));
-//        System.out.println("4 should have 5 ways = " + ClimbingStairs.climbStairs(4));
-//        System.out.println("5 should have 8 ways = " + ClimbingStairs.climbStairs(5));
-//        System.out.println("7 should have 21 ways = " + ClimbingStairs.climbStairs(7));
-
-//        ReverseList.reverseList(ListNode);
     }
 }
---
+++ src/ShuffleTheArray.java
@@ -0,0 +1,12 @@
+public class ShuffleTheArray {
+    /**
+     * Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+     *
+     * Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+     */
+
+    public static int[] shuffle(int[] arr, int n){
+
+        return arr;
+    }
+}
