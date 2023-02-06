2b8ec24ed6e7e7a8a7bb74cac07712bb1ffb7427
Chase Martinez <chasemartinez22@gmail.com>
2023-02-06 16:57:41 -0600

    Feat: Fully operational and performant solution to exercise

--- src/ShuffleTheArray.java
+++ src/ShuffleTheArray.java
@@ -8,9 +8,11 @@ public class ShuffleTheArray {
      */
 
     public static int[] shuffle(int[] nums, int n) {
+        // Create a new array based off of how many xn / yn we're shuffling
         int[] newArray = new int[n * 2];
-        int j = 0;
-        for (int i = 0; i < newArray.length; i += 2, j++) {
+        // Utilizing i to keep track of each pair and j to keep track of
+        // index references within each pair
+        for (int i = 0, j = 0; i < newArray.length; i += 2, j++) {
             newArray[i] = nums[j];
             newArray[i + 1] = nums[n + j];
         }
