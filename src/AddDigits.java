e8a9b152cb2b3e6ebe0cd1e5fed6def65c8d7eea
Chase Martinez <chasemartinez22@gmail.com>
2023-02-07 13:42:55 -0600

    Add: Exercise AddDigits

---
+++ src/AddDigits.java
@@ -0,0 +1,9 @@
+public class AddDigits {
+
+//    Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+    public static int addDigits(int num) {
+
+        return -1;
+    }
+}
