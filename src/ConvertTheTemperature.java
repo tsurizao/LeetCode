ee26712d5d5be474c7c97b8a92994f3958800edb
Chase Martinez <chasemartinez22@gmail.com>
2023-02-10 16:21:37 -0600

    Refactor: reduced solution down to one line

--- src/ConvertTheTemperature.java
+++ src/ConvertTheTemperature.java
@@ -6,9 +6,6 @@ public class ConvertTheTemperature {
 //    Return the array ans. Answers within 10-5 of the actual answer will be accepted.
 
     public static double[] convertTemperature(double celsius) {
-        double[] ans = new double[2];
-        ans[0] = celsius + 273.15;
-        ans[1] = celsius * 1.8 + 32;
-        return ans;
+        return new double[]{celsius + 273.15, celsius * 1.8 + 32};
     }
 }
