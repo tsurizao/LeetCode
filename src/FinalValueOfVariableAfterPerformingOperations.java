379b0b5f88eaaf0e327d5eba31a7bd71bba5532d
Chase Martinez <chasemartinez22@gmail.com>
2023-02-14 15:00:13 -0600

    Feat: Operational solution to exercise

--- src/FinalValueOfVariableAfterPerformingOperations.java
+++ src/FinalValueOfVariableAfterPerformingOperations.java
@@ -7,6 +7,15 @@ public class FinalValueOfVariableAfterPerformingOperations {
 //    Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
 
     public static int finalValueAfterOperations(String[] operations){
+        int x = 0;
+        for (String operation : operations) {
+            if (operation.equalsIgnoreCase("--x") || operation.equalsIgnoreCase("x--")) {
+                x--;
+            } else {
+                x++;
+            }
 
+        }
+        return x;
     }
 }
