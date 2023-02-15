ff75b544c3b3eb1a46c4e65e088591fba6e40585
Chase Martinez <chasemartinez22@gmail.com>
2023-02-15 15:10:24 -0600

    Add: New exercise NumberOfGoodPairs.java

---
+++ src/NumberOfGoodPairs.java
@@ -0,0 +1,10 @@
+public class NumberOfGoodPairs {
+
+    // Given an array of integers nums, return the number of good pairs.
+    //
+    //A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+    public static int numIdenticalPairs(int[] nums) {
+        return -1;
+    }
+}
