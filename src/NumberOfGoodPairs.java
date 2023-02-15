4953f4c2684c292bf5e3ca0e9f5b05c624fda193
Chase Martinez <chasemartinez22@gmail.com>
2023-02-15 15:14:54 -0600

    Feat: Operational solution to exercise

--- src/NumberOfGoodPairs.java
+++ src/NumberOfGoodPairs.java
@@ -5,6 +5,14 @@ public class NumberOfGoodPairs {
     //A pair (i, j) is called good if nums[i] == nums[j] and i < j.
 
     public static int numIdenticalPairs(int[] nums) {
-        return -1;
+        int goodPairs = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if(nums[i] == nums[j] && i < j){
+                    goodPairs++;
+                }
+            }
+        }
+        return goodPairs;
     }
 }
