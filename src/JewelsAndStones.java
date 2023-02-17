b322422e1df46d0edd568dc85a5645c9323f6523
Chase Martinez <chasemartinez22@gmail.com>
2023-02-17 15:57:13 -0600

    Add: Exercise JewelsAndStones.java

---
+++ src/JewelsAndStones.java
@@ -0,0 +1,15 @@
+public class JewelsAndStones {
+
+    /**
+     * You're given strings jewels representing the types of stones that are jewels,
+     * and stones representing the stones you have. Each character in stones is a
+     * type of stone you have. You want to know how many of the stones you have are
+     * also jewels.
+     *
+     * Letters are case sensitive, so "a" is considered a different type of stone from "A".
+     */
+
+    public static int numJewelsInStones(String jewels, String stones){
+        return -1;
+    }
+}
