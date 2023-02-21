debf8e64b970eaf7256c748528658ea4e0913d5a
Chase Martinez <chasemartinez22@gmail.com>
2023-02-21 13:42:19 -0600

    Add new exercise RunningSumOf1DArray.java

---
+++ src/RunningSumOf1DArray.java
@@ -0,0 +1,8 @@
+public class RunningSumOf1DArray {
+    // Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+    //
+    //Return the running sum of nums.
+
+    public static int[] runningSum(int[] nums){
+    }
+}
